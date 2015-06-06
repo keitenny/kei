ef29d8b960b8a9a02d2120638903a542bc40d10c
kei <kei_620@nifty.com>
2015-06-06 23:00:18 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kei::Application.config.secret_key_base = 'f73b0c360e0f8924e51d436325604f05fbf40f18e2369fe9215a53f2ab6d17570b3189514f706efa9ca1c7e4f711f302d49e277e2ce66fe54552b6638e073a82'
